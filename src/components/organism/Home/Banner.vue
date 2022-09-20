c687b24d9400b389249339ad57304f5bb3e255cb
AmienulRana <amienulrana@gmail.com>
2022-09-20 21:48:49 +0700

    membuat componen banner

---
+++ src/components/organism/Home/Banner.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="relative">
+        <img src="../../../assets/image/banner.png" alt="banner png" class="w-full object-cover rounded-xl" />
+        <div class="absolute top-24 left-12">
+            <h1 class="text-4xl font-semibold mb-3.5 text-brown-500">Discount 50% Off <br /> All Members</h1>
+            <p class="text-brown-500">
+                Included tax pricing for U.S citizen or other countries
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Banner'
+    }
+</script>
+
+<style scoped>
+
+</style>
