27b5018e2f0d672974bb17a12bad54c1f2071844
AmienulRana <amienulrana@gmail.com>
2022-09-20 12:04:23 +0700

    menambakan komponen button supaya bisa di reusable

---
+++ src/components/element/Button.vue
@@ -0,0 +1,18 @@
+<template>
+    <button class="flex justify-center rounded-md font-bold text-base items-center text-white bg-green-500 py-2.5 px-8">
+        {{ text }}
+    </button>
+</template>
+
+<script>
+    export default {
+        name:"Button",
+        props: {
+            text:String, 
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
