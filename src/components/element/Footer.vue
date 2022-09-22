15a36e60b375db582b47c431ec27a921cd429e47
AmienulRana <amienulrana@gmail.com>
2022-09-22 11:30:00 +0700

    membuat kmponen footer

---
+++ src/components/element/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="text-center border-t border-grey-500">
+        <p class="text-grey-600 my-8">2022 Copyright Store. All Rights Reserved.</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"Footer"
+    }
+</script>
+
+<style scoped>
+
+</style>
