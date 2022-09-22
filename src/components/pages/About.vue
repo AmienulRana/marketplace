3cc3bd35f679f640c8117a6436e6564d8bf381bb
AmienulRana <amienulrana@gmail.com>
2022-09-22 12:42:14 +0700

    memindahkan folder page ke folder komponen

---
+++ src/components/pages/About.vue
@@ -0,0 +1,24 @@
+<template> 
+    <Navbar />
+    <router-view>
+        <div>
+            <h2>Ini adalah halaman About</h2>
+        </div>
+    </router-view>
+    <Footer />
+</template>
+
+<script>
+    import Navbar from '../components/element/Navbar/Navbar.vue'
+    import Footer from '../components/element/Footer.vue'
+    export default {
+        name: 'About',
+        components:{
+            Navbar, Footer
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/pages/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <Navbar />
+    <router-view>
+        <Banner />
+        <Category />
+        <NewProducts />
+    </router-view>
+    <Footer />
+</template>
+
+<script>
+    import Navbar from '../components/element/Navbar/Navbar.vue'
+    import Banner from '../components/organism/Home/Banner.vue'
+    import Category from '../components/organism/Home/Category.vue'
+    import NewProducts from '../components/organism/Home/NewProduct.vue'
+    import Footer from '../components/element/Footer';
+    export default {
+        name: 'Home',
+        components:{
+            Navbar, Banner, Category, NewProducts, Footer
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
