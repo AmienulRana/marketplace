fbf01508e7c7e5e00c20cd5e0c179c866bcad3e1
AmienulRana <amienulrana@gmail.com>
2022-09-24 13:08:50 +0700

    membuat halaman dashboard

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+    <h1>Hello Content</h1>
+  </Layout>
+</template>
+
+<script>
+import Layout from "../components/Layout/Dashboard.vue";
+export default {
+  name: "Dahsboard",
+  components: { Layout },
+};
+</script>
+
+<style scoped>
+</style>
