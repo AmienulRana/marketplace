df71210c6029afc2b88d961e15ef49d8cd198110
AmienulRana <amienulrana@gmail.com>
2022-09-24 18:58:40 +0700

    menambahkan layout ada halaman 404

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,17 +1,16 @@
 <template>
-  <Navbar />
-  <h2 class="text-center text-grey-600 text-2xl my-20">
-    404 | Opps! Halaman yang anda kunjungi tidak ditemukan
-  </h2>
-  <Footer />
+  <Layout>
+    <h2 class="text-center text-grey-600 text-2xl my-20">
+      404 | Opps! Halaman yang anda kunjungi tidak ditemukan
+    </h2>
+  </Layout>
 </template>
 
 <script>
-import Navbar from "../components/element/Navbar/Navbar.vue";
-import Footer from "../components/element/Footer";
+import Layout from "../components/Layout";
 export default {
   name: "NotFound",
-  components: { Navbar, Footer },
+  components: { Layout },
 };
 </script>
 
