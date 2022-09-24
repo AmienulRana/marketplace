66a0420ab8d97ab5480818741e966510f6578fea
AmienulRana <amienulrana@gmail.com>
2022-09-24 13:13:09 +0700

    mengubah tinggi dair banner dan menyesuaikan position Tagline agar responsive

--- src/components/organism/Home/Banner.vue
+++ src/components/organism/Home/Banner.vue
@@ -1,21 +1,27 @@
 <template>
-    <div class="relative h-80">
-        <img src="../../../assets/image/banner.png" alt="banner png" class="w-full h-full object-cover rounded-xl" />
-        <div class="absolute top-24 left-12">
-            <h1 class="text-4xl font-semibold mb-3.5 text-brown-500">Discount 50% Off <br /> All Members</h1>
-            <p class="text-brown-500">
-                Included tax pricing for U.S citizen or other countries
-            </p>
-        </div>
+  <div class="relative h-80">
+    <img
+      src="../../../assets/image/banner.png"
+      alt="banner png"
+      class="w-full h-full object-cover rounded-xl"
+    />
+    <div class="md:top-24 md:left-12 absolute top-10 left-8">
+      <h1 class="text-4xl font-semibold mb-3.5 text-brown-500">
+        Discount 50% Off <br />
+        All Members
+      </h1>
+      <p class="text-brown-500">
+        Included tax pricing for U.S citizen or other countries
+      </p>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name:'Banner'
-    }
+export default {
+  name: "Banner",
+};
 </script>
 
 <style scoped>
-
 </style>
