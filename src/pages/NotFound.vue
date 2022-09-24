8fdc33a81f1beef1333665281425254306735600
AmienulRana <amienulrana@gmail.com>
2022-09-24 13:09:32 +0700

    menghandle page 404 dengan membuat halaman NotFound

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <Navbar />
+  <h2 class="text-center text-grey-600 text-2xl my-20">
+    404 | Opps! Halaman yang anda kunjungi tidak ditemukan
+  </h2>
+  <Footer />
+</template>
+
+<script>
+import Navbar from "../components/element/Navbar/Navbar.vue";
+import Footer from "../components/element/Footer";
+export default {
+  name: "NotFound",
+  components: { Navbar, Footer },
+};
+</script>
+
+<style scoped>
+</style>
