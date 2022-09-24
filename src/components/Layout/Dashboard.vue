1e97f9958c1441cf8937bf9cc8cf316aebb4f585
AmienulRana <amienulrana@gmail.com>
2022-09-24 13:12:05 +0700

    membuat layout untuk dashboard kaena banyak komponen yang berulang seperti sidebar dan navbar

---
+++ src/components/Layout/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex">
+    <Sidebar />
+    <slot />
+  </div>
+</template>
+
+<script>
+import Sidebar from "../element/Sidebar.vue";
+export default {
+  name: "DashboardLayout",
+  components: { Sidebar },
+};
+</script>
+
+<style scoped>
+</style>
