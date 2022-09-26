f2f40849ce7b3b735f4451a34786bac6d2009a44
AmienulRana <amienulrana@gmail.com>
2022-09-26 09:57:08 +0700

    memindahkan baris last transaction ke file RowTransaction karena akandigunakan di file transaction

---
+++ src/components/organism/RowTransaction.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex mb-4 items-center justify-between p-3.5 bg-white rounded-md">
+    <div class="flex items-center">
+      <img
+        src="../../public/image/newproduct1.jpg"
+        class="w-11 h-11 object-cover rounded-md mr-3"
+      />
+      <h2>Shirup Marzzan</h2>
+    </div>
+    <p>Angga Risky</p>
+    <p>12 Januari, 2020</p>
+    <img src="../assets/icons/arrow-right.svg" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RowTransaction",
+};
+</script>
+
+<style scoped>
+</style>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,14 +1,32 @@
 <template>
   <Layout>
-    <h1>Hello Content</h1>
+    <section class="sm:grid-cols-3 grid mb-6">
+      <div class="md:h-32 sm:w-11/12 bg-white mb-2 w-full rounded-xl p-6">
+        <p class="text-grey-600">Customer</p>
+        <h2 class="md:text-3xl text-2xl font-bold text-blue-500">15,029</h2>
+      </div>
+      <div class="md:h-32 sm:w-11/12 bg-white mb-2 w-full rounded-xl p-6">
+        <p class="text-grey-600">Revenue</p>
+        <h2 class="md:text-3xl text-2xl font-bold text-blue-500">$931,290</h2>
+      </div>
+      <div class="md:h-32 sm:w-11/12 bg-white mb-2 w-full rounded-xl p-6">
+        <p class="text-grey-600">Transaction</p>
+        <h2 class="md:text-3xl text-2xl font-bold text-blue-500">22,409,399</h2>
+      </div>
+    </section>
+    <section>
+      <h2 class="text-blue-500 mb-3.5 text-lg">Recent Transactions</h2>
+      <RowTransaction />
+    </section>
   </Layout>
 </template>
 
 <script>
 import Layout from "../components/Layout/Dashboard.vue";
+import RowTransaction from "../components/organism/RowTansaction.vue";
 export default {
   name: "Dahsboard",
-  components: { Layout },
+  components: { Layout, RowTransaction },
 };
 </script>
 
