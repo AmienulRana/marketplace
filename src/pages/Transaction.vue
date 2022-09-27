c46e60fba1a29b157b5f4ce01c89688fe4debd47
AmienulRana <amienulrana@gmail.com>
2022-09-27 19:35:38 +0700

    membuat halamn transaction

---
+++ src/pages/Transaction.vue
@@ -0,0 +1,23 @@
+<template>
+  <Layout title="Transactions" text="Big result start from the small one">
+    <div class="flex mb-5">
+      <p class="cursor-pointer font-bold border-b-2 mr-8 border-black">
+        Sell Product
+      </p>
+      <p class="cursor-pointer">Buy Product</p>
+    </div>
+    <RowTransaction />
+  </Layout>
+</template>
+
+<script>
+import Layout from "../components/Layout/Dashboard.vue";
+import RowTransaction from "../components/organism/RowTransaction.vue";
+export default {
+  name: "Transaction",
+  components: { Layout, RowTransaction },
+};
+</script>
+
+<style scoped>
+</style>
