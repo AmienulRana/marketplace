fbb17a208a08d596242d369aeb52c6e058cb1966
AmienulRana <amienulrana@gmail.com>
2022-09-28 12:10:55 +0700

    no spesification commit

---
+++ src/components/element/Textarea.vue
@@ -0,0 +1,29 @@
+<template>
+  <label class="text-blue-500">{{ label }}</label>
+  <textarea
+    class="
+      w-full
+      bg-grey-400
+      p-2.5
+      outline-none
+      border-2 border-transparent
+      duration-300
+      focus:border-green-400
+      rounded-md
+      mt-1.5
+      resize-none
+    "
+    rows="4"
+  >
+  </textarea>
+</template>
+
+<script>
+export default {
+  name: "Textarea",
+  props: { label: String },
+};
+</script>
+
+<style scoped>
+</style>
