fb1a497eba2601a4341751e5b244adbe4fe46efb
AmienulRana <amienulrana@gmail.com>
2022-10-03 18:22:49 +0700

    membuat agar custom select component bisa mengubah model yang ada di parent

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -5,9 +5,26 @@
         Memulai untuk jual beli <br />
         dengan cara terbaru
       </h1>
-      <Input label="Full Name" class="mb-3.5" />
-      <Input label="Email Address" class="mb-3.5" />
-      <Input label="Password" type="password" class="mb-3.5" />
+
+      <Input
+        label="Full Name"
+        class="mb-3.5"
+        :modelValue="fullname"
+        @update:modelValue="(newValue) => (fullname = newValue)"
+      />
+      <Input
+        label="Email Address"
+        class="mb-3.5"
+        :modelValue="email"
+        @update:modelValue="(newValue) => (email = newValue)"
+      />
+      <Input
+        label="Password"
+        type="password"
+        class="mb-3.5"
+        :modelValue="password"
+        @update:modelValue="(newValue) => (password = newValue)"
+      />
       <p class="text-blue-500">Store</p>
       <p class="text-grey-600">Apakah anda juga ingin membuka toko?</p>
       <div class="flex">
@@ -20,13 +37,23 @@
         <Radio label="Enggak, makasih" id="tidak" @change="changeValueStore" />
       </div>
       <template v-if="openAStore">
-        <Input label="Nama Toko" class="mb-3.5" />
-        <Select label="Category" :options="categorys" />
+        <Input
+          label="Nama Toko"
+          class="mb-3.5"
+          :modelValue="nama_toko"
+          @update:modelValue="(newValue) => (nama_toko = newValue)"
+        />
+        <Select
+          label="Category"
+          :options="categorys"
+          :modelValue="category"
+          @update:modelValue="(newValue) => (category = newValue)"
+        />
       </template>
       <Button
         text="Sign In to My Account"
         class="mt-7 mb-3.5 w-full"
-        @click="getValueStore"
+        @click="handleToRegister"
       />
       <router-link to="/register">
         <Button text="Sign up" class="bg-grey-500 text-grey-700 w-full" />
@@ -46,16 +73,33 @@ export default {
   components: { Layout, Input, Button, Radio, Select },
   data() {
     return {
-      openAStore: false,
       categorys: ["Funiture", "Baby", "Tools", "Gadgets"],
+      openAStore: false,
+      fullname: "",
+      email: "",
+      password: "",
+      category: "",
+      nama_toko: "",
     };
   },
   methods: {
     changeValueStore() {
+      if (!this.openAStore) {
+        this.nama_toko = "";
+        this.category = "";
+      }
       this.openAStore = this.openAStore ? false : true;
     },
-    getValueStore() {
-      console.log(this.openAStore);
+    handleToRegister() {
+      const data = {
+        fullname: this.fullname,
+        email: this.email,
+        password: this.password,
+        openStore: this.openAStore,
+        category: this.category,
+        nama_toko: this.nama_toko,
+      };
+      console.log(data);
     },
   },
 };
