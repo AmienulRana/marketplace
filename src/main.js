abf562a881e85ffa395ae34312ef505092e4499e
AmienulRana <amienulrana@gmail.com>
2022-10-05 09:58:02 +0700

    meregistrasikan plugin vue-toastfiaction dan font-awesome agar bisa digunakan

--- src/main.js
+++ src/main.js
@@ -2,4 +2,17 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/styles/index.css";
 import router from "./router";
-createApp(App).use(router).mount("#app");
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import Toast from "vue-toastification";
+// Import the CSS or use your own!
+import "vue-toastification/dist/index.css";
+
+library.add(faMinus, faPlus);
+
+createApp(App)
+  .use(router)
+  .use(Toast)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
