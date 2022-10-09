e0abb1919974bdb40cf8e3b48a45b17c31b31f6e
AmienulRana <amienulrana@gmail.com>
2022-10-09 15:30:21 +0700

    membuat komponen untuk user yang sudah login

---
+++ src/components/element/Navbar/NavbarLogin.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="md:border-l-2 relative flex pl-2 border-grey-500">
+      <router-link to="/dashboard">
+        <p class="text-blue-500 flex">
+          Hi, <span class="sm:block hidden ml-2">Amienul</span>
+        </p>
+      </router-link>
+      <router-link to="/cart">
+        <img src="../../../assets/icons/shopping.svg" class="ml-4" />
+        <p
+          class="
+            absolute
+            top-3
+            -right-2
+            bg-green-500
+            w-4
+            h-4
+            rounded-full
+            flex
+            items-center
+            justify-center
+            p-2
+            text-white
+          "
+        >
+          3
+        </p>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavbarLogin",
+};
+</script>
+
+<style scoped>
+</style>
