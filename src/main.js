60cf5d5a69e53b1b27422d0f61b5ce464d084328
AmienulRana <amienulrana@gmail.com>
2022-10-09 15:38:06 +0700

    menambahkan icon faLocationDot

--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@ import App from "./App.vue";
 import "./assets/styles/index.css";
 import router from "./router";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+import {
+  faLocationDot,
+  faMinus,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import Toast from "vue-toastification";
 // Import the CSS or use your own!
 import "vue-toastification/dist/index.css";
 
-library.add(faMinus, faPlus);
+library.add(faMinus, faPlus, faLocationDot);
 
 createApp(App)
   .use(router)
