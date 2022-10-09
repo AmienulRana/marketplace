60653cf9c6868d2d77f7402f30fb890de14292e4
AmienulRana <amienulrana@gmail.com>
2022-10-09 17:34:38 +0700

    memindahkan beberapa model yang tidak berhubungan dengan model backend dalam satu halaman ke store setting, agar tidak susah untuk mengupdate beberapa model pada backend

--- src/pages/MyAccount.vue
+++ src/pages/MyAccount.vue
@@ -2,24 +2,19 @@
   <Layout title="My Account" text="Update your current profile">
     <section class="bg-white rounded-lg h-auto w-full p-10 relative">
       <div class="md:grid-cols-2 grid gap-4 grid-cols-1 mb-6">
-        <Input label="Your Name" />
-        <Input label="Your Email" />
-      </div>
-      <div class="md:grid-cols-2 grid gap-4 grid-cols-1 mb-6">
-        <Input label="Address 1" />
-        <Input label="Addres 2" />
-      </div>
-      <div class="md:grid-cols-3 grid gap-3 grid-cols-1 mb-6">
-        <Input label="Province" />
-        <Input label="City" />
-        <Input label="Postal Code" />
-      </div>
-      <div class="md:grid-cols-2 grid gap-4 grid-cols-1 mb-6">
-        <Input label="Country" />
-        <Input label="Mobile" />
+        <Input
+          label="Your Name*"
+          :modelValue="fullname"
+          @update:modelValue="(newValue) => (fullname = newValue)"
+        />
+        <Input
+          label="Your Email*"
+          :modelValue="email"
+          @update:modelValue="(newValue) => (email = newValue)"
+        />
       </div>
       <div class="flex justify-end">
-        <Button class="float-right" text="Save now" />
+        <Button text="Save now" />
       </div>
     </section>
   </Layout>
@@ -31,13 +26,28 @@ import Input from "../components/element/Input.vue";
 import Select from "../components/element/Select.vue";
 import Radio from "../components/element/Radio.vue";
 import Button from "../components/element/Button.vue";
+import jwt_decode from "jwt-decode";
+
 export default {
   name: "StoreSetting",
   components: { Layout, Input, Select, Radio, Button },
   data() {
     return {
       options: ["Funiture", "Baby", "Tools", "Gadgets"],
+      fullname: "",
+      email: "",
     };
+  },
+
+  methods: {},
+  mounted() {
+    const decodeJwtToken = () => {
+      const token = localStorage.getItem("token");
+      const { fullname, email } = jwt_decode(token);
+      this.fullname = fullname;
+      this.email = email;
+    };
+    decodeJwtToken();
   },
 };
 </script>
