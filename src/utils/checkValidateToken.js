9abc333c9392e92deab43731b4c1b4e359c40a47
AmienulRana <amienulrana@gmail.com>
2022-10-12 11:48:53 +0700

    menambahkan kondisi jika response status lebih dar 200 dan leih kecil dari 300

--- src/utils/checkValidateToken.js
+++ src/utils/checkValidateToken.js
@@ -2,6 +2,8 @@ const checkValidateToken = (response, toast, router) => {
   if (response?.status === 401 && !response?.data?.auth) {
     toast.error(response?.data?.message);
     return router.push({ name: "login" });
+  } else if (response?.status >= 200 && response?.status < 300) {
+    toast.success(response?.data?.message);
   } else if (response?.status > 400) {
     toast.error(response?.data?.message);
   }
