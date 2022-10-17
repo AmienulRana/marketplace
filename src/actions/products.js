f0d072158717f26437234b74616aa4d2419f6d51
AmienulRana <amienulrana@gmail.com>
2022-10-17 21:55:43 +0700

    menambahkan function getProductAPI

--- src/actions/products.js
+++ src/actions/products.js
@@ -16,20 +16,17 @@ export const configOptionsAPI = (method, stateToken, payload) => {
   return options;
 };
 
-// export const getMyStoreAPI = async ({ token: stateToken }) => {
-//   const token = stateToken || CONFIG.token;
-//   try {
-//     const response = await axios.get(`${CONFIG.URL_API}/store`, {
-//       headers: { Authorization: `Bearer ${token}` },
-//     });
-//     return response?.data;
-//   } catch (error) {
-//     return error.response;
-//   }
-// };
+export const getProductAPI = async (token) => {
+  try {
+    const options = configOptionsAPI("get", token, {});
+    const response = await axios(options);
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
 
 export const addProductAPI = async (payload, { token: stateToken }) => {
-  //   const options = configOptionsAPI("post", stateToken, payload);
   const token = CONFIG.token || stateToken;
   try {
     const response = await axios.post(`${CONFIG.URL_API}/products`, payload, {
