42f91b82881bb7d6894d0180d9f3b1e87385d3e1
AmienulRana <amienulrana@gmail.com>
2022-10-21 15:10:49 +0700

    membuat route untuk edit product

--- src/router/index.js
+++ src/router/index.js
@@ -12,12 +12,14 @@ import Login from "../pages/Login.vue";
 import Register from "../pages/Register.vue";
 import RegisterSuccess from "../pages/RegisterSuccess.vue";
 import Cart from "../pages/Cart.vue";
+import EditProduct from "../pages/EditProduct.vue";
 const routes = [
   { path: "/", component: Home, name: "index" },
   { path: "/detail/:id", component: Detail },
   { path: "/dashboard", component: Dashboard },
   { path: "/transactions", component: Transactions },
   { path: "/my-products", component: MyProduct },
+  { path: "/my-products/:id", component: EditProduct },
   { path: "/settings", component: StoreSetting },
   { path: "/my-account", component: MyAccount },
   { path: "/add-product", component: AddProduct },
