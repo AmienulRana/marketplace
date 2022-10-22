ce859c74d7a08634fb56f12cc24ce300e0d421ea
AmienulRana <amienulrana@gmail.com>
2022-10-22 11:32:06 +0700

    menambahkan kondisi untuk meredirect user otomatis dalam waktu 4 detik

--- src/pages/RegisterSuccess.vue
+++ src/pages/RegisterSuccess.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="flex justify-center items-center flex-col min-h-screen">
     <img src="../assets/icons/success.svg" />
-    <h1 class="text-2xl text-blue-500 mt-10 mb-3.5">Welcome to Store</h1>
+    <h1 class="text-2xl text-blue-500 mt-10 mb-3.5">Register Success</h1>
     <p class="text-grey-600">
       Kamu sudah berhasil terdaftar <br />
       bersama kami. Let’s grow up now.
     </p>
-    <router-link to="/dashboard">
-      <Button text="My Dashboard" class="mt-24 mb-3.5" />
-    </router-link>
-    <router-link to="/">
-      <Button text="Go To Shopping" class="bg-grey-500 text-grey-700" />
-    </router-link>
+    <p class="mt-3.5 text-grey-600">
+      Anda akan di redirect ke halaman Login secara otomatis dalam
+      <span class="text-green-500">{{ countTiming }}</span> detik.
+    </p>
   </div>
 </template>
 
@@ -20,6 +18,21 @@ import Button from "../components/element/Button.vue";
 export default {
   name: "RegisterSuccess",
   components: { Button },
+  data() {
+    return {
+      countTiming: 4,
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      if (this.countTiming === 0) {
+        this.$router.push({ name: "success" });
+        this.countTiming = 4;
+      } else {
+        this.countTiming = this.countTiming - 1;
+      }
+    }, 1000);
+  },
 };
 </script>
 
