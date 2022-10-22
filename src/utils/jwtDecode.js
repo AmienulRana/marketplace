4a3a0a89cf6ad29a622b09eeb0c7e9445a765af3
AmienulRana <amienulrana@gmail.com>
2022-10-22 19:17:09 +0700

    meredirect user ketika tidak ada token yang berhasil di decode

--- src/utils/jwtDecode.js
+++ src/utils/jwtDecode.js
@@ -1,8 +1,12 @@
 import jwtDecode from "jwt-decode";
 const decodeJwtToken = () => {
   const token = localStorage.getItem("token");
-  const payload = jwtDecode(token);
-  return payload;
+  if (!token) {
+    window.location.href = "/login";
+  } else {
+    const payload = jwtDecode(token);
+    return payload;
+  }
 };
 
 export default decodeJwtToken;
