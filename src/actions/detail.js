5fae75abbbce0ecf22ee7682c3dfbf9d986f7cee
AmienulRana <amienulrana@gmail.com>
2022-10-30 17:50:14 +0700

    menyimpan semua panggilan api dalam halaman detail di file ini

---
+++ src/actions/detail.js
@@ -0,0 +1,30 @@
+import CONFIG from "@/config";
+import optionsAxios from "@/utils/optionsAxios";
+import axios from "axios";
+
+export const checkOngkirAPI = async (data) => {
+  try {
+    const response = await axios.post(`${CONFIG.URL_API}/user/check-ongkir`, {
+      ...data,
+    });
+    const expensivePrice = [];
+    const rangeOngkir = response?.data?.results[0]?.costs;
+    rangeOngkir.map((range) =>
+      range.cost.map((ongkir) => expensivePrice.push(ongkir.value))
+    );
+    const sortThePrice = expensivePrice.sort((a, b) => a - b);
+    return sortThePrice[0];
+  } catch (error) {
+    return error.response;
+  }
+};
+
+export const addToCartAPI = async (token, payload) => {
+  try {
+    const options = optionsAxios("post", token, payload, "/cart");
+    const response = await axios(options);
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
