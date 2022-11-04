7ec56d41a4fabe72999e82196e503ddce1c9a91c
AmienulRana <amienulrana@gmail.com>
2022-11-04 20:04:54 +0700

    menambahkan method checkOngkir

--- src/actions/cart.js
+++ src/actions/cart.js
@@ -1,5 +1,7 @@
 import optionsAxios from "@/utils/optionsAxios";
 import axios from "axios";
+import CONFIG from "@/config";
+
 export const getMyCartsAPI = async (token) => {
   try {
     const options = optionsAxios("get", token, {}, "/cart");
@@ -15,6 +17,18 @@ export const deleteMyCartsAPI = async (token, id) => {
   try {
     const options = optionsAxios("delete", token, {}, `/cart/${id}`);
     const response = await axios(options);
+    return response;
+  } catch (error) {
+    console.log(error);
+    return error.response;
+  }
+};
+
+export const checkOngkirAPI = async (data) => {
+  try {
+    const response = await axios.post(`${CONFIG.URL_API}/user/check-ongkir`, {
+      ...data,
+    });
     return response;
   } catch (error) {
     console.log(error);
