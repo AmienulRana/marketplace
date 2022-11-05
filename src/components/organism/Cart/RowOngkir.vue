d3b6b96659d539145bd726539a05b8a15c486a0c
AmienulRana <amienulrana@gmail.com>
2022-11-05 19:47:54 +0700

    membuat komponen RowOngkir untuk merefactor kode pada halaman card agar lebih rapi da muda dibaca

---
+++ src/components/organism/Cart/RowOngkir.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    class="
+      sm:p-5
+      p-2.5
+      bg-gray-100
+      mb-3.5
+      cursor-pointer
+      rounded-sm
+      hover:bg-gray-200
+      duration-200
+      border-l-4 border-green-500
+    "
+  >
+    <p class="flex items-center">
+      <span class="font-bold mr-6">{{ service_name }}</span>
+      <VueNumberFormat
+        :value="value"
+        class="w-36 text-green-500 bg-transparent"
+        disabled="true"
+      ></VueNumberFormat>
+    </p>
+    <p class="text-grey-600">estimasi paket akan sampai {{ etd }} hari</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RowOngkir",
+  props: {
+    service_name: { type: String },
+    value: { type: Number },
+    etd: { type: String },
+  },
+};
+</script>
+
+<style scoped>
+</style>
