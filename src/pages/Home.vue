f97b56adee2bd7813d9d718a920721d98aacd685
AmienulRana <amienulrana@gmail.com>
2022-11-07 20:27:22 +0700

    menambahkan function untuk nge-set token yang ada di store(vuex) ketika pertama kali masuk ke halaman, dan user sudah login

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -11,6 +11,8 @@ import Layout from "../components/Layout";
 import Banner from "../components/organism/Home/Banner.vue";
 import Category from "../components/organism/Home/Category.vue";
 import NewProducts from "../components/organism/Home/NewProduct.vue";
+import CONFIG from "@/config";
+
 export default {
   name: "Home",
   components: {
@@ -18,6 +20,12 @@ export default {
     Category,
     NewProducts,
     Layout,
+  },
+  mounted() {
+    const setToken = () => {
+      this.$store.commit("storeTheToken", CONFIG.token);
+    };
+    setToken();
   },
 };
 </script>
