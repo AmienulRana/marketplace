94dcec61dec60a037b2101875d7bcf630d25483e
AmienulRana <amienulrana@gmail.com>
2022-11-08 21:10:31 +0700

    menambahkan kondisi untuk value dengan tipe data number dan string

--- src/components/organism/TransactionDetail/TextInformation.vue
+++ src/components/organism/TransactionDetail/TextInformation.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="md:block mb-6 flex justify-between">
     <p class="md:mb-2 text-sm text-grey-600">{{ title }}</p>
-    <p class="md:text-lg text-sm text-blue-500 truncate">{{ value }}</p>
+    <p class="md:text-lg text-sm text-blue-500 truncate">
+      <template v-if="typeof value === 'string'">
+        {{ value }}
+      </template>
+      <VueNumberFormat
+        :value="value"
+        class="md:text-left w-32 bg-transparent text-right"
+        disabled="true"
+        v-else
+      ></VueNumberFormat>
+    </p>
   </div>
 </template>
 
