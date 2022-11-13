537dfe60177f60c45acf6db6a249194ff6aa17fc
AmienulRana <amienulrana@gmail.com>
2022-11-13 19:38:03 +0700

    menambahkan actions untuk menghandle request total product yang ada di kerajang

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import { getMyCartsAPI } from "@/actions/cart";
 import { createStore } from "vuex";
 import location from "./modules/location";
 
@@ -5,6 +6,7 @@ const store = createStore({
   state() {
     return {
       token: undefined,
+      total_cart: 0,
     };
   },
   mutations: {
@@ -14,6 +16,15 @@ const store = createStore({
     removeTheToken(state) {
       state.token = undefined;
     },
+    setTotalCart(state, payload){
+      state.total_cart = payload;
+    }
+  },
+  actions:{
+    async getTotalCart(context){
+      const response = await getMyCartsAPI(this.state.token); 
+      return context.commit('setTotalCart', response?.data?.products?.length || 0);
+    }
   },
   modules: {
     location,
