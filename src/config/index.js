c56433333fd1b3c138807fc21a0680291dec94bb
AmienulRana <amienulrana@gmail.com>
2022-11-17 11:10:39 +0700

    menambahkan url production api

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,10 @@
 const MODE = 'dev';
 
-const URL = MODE === 'dev' ? "http://localhost:5000/api/v1" : ''
+const URL = MODE === 'dev' ? "http://localhost:5000/api/v1" : 'https://api-marteplace.vercel.app/api/v1'
 
 const CONFIG = {
   URL_API: URL ,
-  URL_IMAGES: MODE === 'dev' ? "http://localhost:5000/images" : '',
+  URL_IMAGES: MODE === 'dev' ? "http://localhost:5000/images" : 'https://api-marteplace.vercel.app/images',
   token: localStorage.getItem("token"),
 };
 
