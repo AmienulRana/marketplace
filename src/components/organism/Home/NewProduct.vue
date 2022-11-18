c8aedadf77368b6ae2a8c3f0ac14a309c09a7e97
AmienulRana <amienulrana@gmail.com>
2022-11-18 20:17:41 +0700

    menambahkan loading ketika new product masih di fecthing

--- src/components/organism/Home/NewProduct.vue
+++ src/components/organism/Home/NewProduct.vue
@@ -1,7 +1,9 @@
 <template>
   <h1 class="text-blue-500 mt-6 mb-4 text-xl font-medium">New Products</h1>
+  <Loading v-if="loading" />
   <section
     class="grid md:grid-cols-4 lg:grid-cols-5 gap-4 sm:grid-cols-3 grid-cols-2"
+    v-else
   >
     <div
       class="mb-6 cursor-pointer"
@@ -39,12 +41,15 @@
 import { getNewProductAPI } from "@/actions/home.js";
 import CONFIG from "@/config";
 import jwtDecode from "jwt-decode";
+import Loading from "@/components/element/Loading.vue";
 
 export default {
   name: "NewProduct",
+  components: { Loading },
   data() {
     return {
       urlImgServe: CONFIG.URL_IMAGES,
+      loading: true,
       products: [],
       store_id: "",
     };
@@ -54,6 +59,7 @@ export default {
       const response = await getNewProductAPI();
       this.products = response?.data;
       const token = localStorage.getItem("token") || this.$store.state.token;
+      this.loading = false;
       if (token) {
         const { store_id } = jwtDecode(token);
         this.store_id = store_id;
